6ed92689cb5b5a1f4c24045b7b3fa774f3bccd7c
wiwiam <68201752+wiwiam@users.noreply.github.com>
2021-01-26 17:30:28 -0600

    Delete js directory

--- js/logout.js
+++
@@ -1,9 +0,0 @@
-//Cerrado de sesion
-function cerrar(){
-	firebase.auth().signOut().then(()=>{
-          window.location.href="index.html";
-	}).catch(error=>{
-		 Materialize.toast('error', 4000)
-	})
-}
-//Cerrado de sesion
